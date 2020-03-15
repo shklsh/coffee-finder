50bea9600f309dabab505fe2e7b933d8d34edd71
Se Oh <seoh@Ses-MacBook-Pro.local>
2020-03-16 03:01:47 +1100

    Updates

---
+++ precache-manifest.2f665b633ebc875c4d0c0aff975f48ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d566405e7ab8a02626886ad1abf265a3",
+    "url": "/coffee-finder/index.html"
+  },
+  {
+    "revision": "c08bc0804283796c1d9f",
+    "url": "/coffee-finder/static/css/2.42611077.chunk.css"
+  },
+  {
+    "revision": "a262b751bc45f86f1487",
+    "url": "/coffee-finder/static/css/main.d68fa30f.chunk.css"
+  },
+  {
+    "revision": "c08bc0804283796c1d9f",
+    "url": "/coffee-finder/static/js/2.0bc8b543.chunk.js"
+  },
+  {
+    "revision": "24bb84a7895da351d4bf6494a07081d0",
+    "url": "/coffee-finder/static/js/2.0bc8b543.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a262b751bc45f86f1487",
+    "url": "/coffee-finder/static/js/main.10a33973.chunk.js"
+  },
+  {
+    "revision": "838e3212eebdf9888737",
+    "url": "/coffee-finder/static/js/runtime-main.69af96f9.js"
+  }
+]);
